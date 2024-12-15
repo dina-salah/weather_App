295c529b7ddf93b6ac2a28d3963df4aa1d2cf14b
dina-salah <dinasalah35586@gmail.com>
2024-12-14 22:55:12 -0700

    first commit

---
+++ city.module.js
@@ -0,0 +1,6 @@
+// export class city {
+//     constructor (name){
+
+
+//     }
+// }
---
+++ index.js
@@ -0,0 +1,26 @@
+import { cityApi } from "./cities.module.js";
+
+
+
+const searchBar = document.getElementById('search-bar');
+const findBtn = document.getElementById('find-btn');
+const weatherBox = document.getElementById('weather-box');
+
+let city_info = [];
+
+searchBar.addEventListener('keyup',function(){
+ let city = new cityApi(searchBar.value);
+ city_info = city.getCity();
+
+ console.log(city_info);
+ 
+})
+
+
+
+
+// search find method letter by letter fetch data 
+// obj { day1, day2 , day3 }
+// using classes day have 6 proberties
+// display from the grobale obj 
+// loading 
